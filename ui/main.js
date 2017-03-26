a74e7e7d06dd2c96f456dd873ef97a7dd3b9736b
harikrishnabs <krishna.krishna323@gmail.com>
2017-03-26 06:31:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,15 +16,15 @@ button.onclick = function () {
                span.innerHTML = counter.toString();
             }
         }
-        //Not done yet
+        // Not done yet
     };
    
-   //Make the request
+   // Make the request
    request.open('GET', 'http://harikrishnabs.imad.hasura-app.io/counter', true);
    request.send(null);
 };
 
-//Submit nmae
+// Submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
@@ -36,23 +36,23 @@ submit.onclick = function () {
     // Capture the response and store it in a variable
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
-            //Take some action
+            // Take some action
             if (request.status === 200) {
-              //Capture a list of name and render it as a list
+              // Capture a list of name and render it as a list
               var names = request.responseText;
               names = JSON.parse(names);
               var list = ``;
-              for (var i=0; i< name.length; i++) {
-                  list+= '<li>' + names[i] + '</li>';
+              for (var i=0; i< names.length; i++) {
+                  list += '<li>' + names[i] + '</li>';
               }
               var ul = document.getElementById('namelist');
               ul.innerHTML = list;
             }
         }
-        //Not done yet
+        // Not done yet
     };
    
-   //Make the request
+   // Make the request
    request.open('GET', 'http://harikrishnabs.imad.hasura-app.io/submit-name?name=' + name, true);
    request.send(null);
    
