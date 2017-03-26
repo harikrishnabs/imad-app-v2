dd148cd1a29c056b3fbc7f8c99c571fe9c23cda2
harikrishnabs <krishna.krishna323@gmail.com>
2017-03-26 05:52:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,20 @@ button.onclick = function () {
    request.open('GET', 'http://harikrishnabs.imad.hasura-app.io/counter', true);
    request.send(null);
 };
+
+//Submit nmae
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+  //Make a Request to thr server and send the name
+  
+  //Capture a list of name and render it as a list
+  var names = ['name1', 'name2', 'name3', 'name4'];
+  var list = ``;
+  for (var i=0; i< name.length; i++) {
+      list+= '<li>' + names[i] + '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+};
